423c32fc51e6b446d88dabe0f8a9277e03671701
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2021-06-04 14:30:55 +0200

    change jsx extensions to js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import Constants from "expo-constants";
-import { AppName } from "./components/AppName.jsx";
-import { Grid } from "./components/Grid.jsx";
-import { NonogramGameProvider } from "./nonogramGame/NonogramGameContext.jsx";
+import { AppName } from "./components/AppName.js";
+import { Grid } from "./components/Grid.js";
+import { NonogramGameProvider } from "./nonogramGame/NonogramGameContext.js";
 import { smiley } from "./nonograms/smiley.js";
-import { CheckGridButton } from "./components/CheckGridButton.jsx";
+import { CheckGridButton } from "./components/CheckGridButton.js";
 
 export default function App() {
   return (
