c3c166186cc9227ce8b7a8d7df12c8f8cc5a0942
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2021-06-06 16:30:58 +0200

    remove useless provider props

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import * as React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar, Platform } from "react-native";
 import Constants from "expo-constants";
 import { AppName } from "./components/AppName.js";
 import { Grid } from "./components/Grid.js";
 import { NonogramGameProvider } from "./nonogramGame/NonogramGameContext.js";
-import { smiley } from "./nonograms/smiley.js";
 import { CheckGridButton } from "./components/CheckGridButton.js";
 import { NonogramPicker } from "./components/NonogramPicker.js";
 import { Spacer } from "./components/Spacer.js";
 
 export default function App() {
   return (
-    <NonogramGameProvider nonogram={smiley}>
-      <StatusBar barStyle="dark-content" />
+    <NonogramGameProvider>
+      <StatusBar
+        barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
+      />
       <View style={styles.container}>
         <AppName />
         <NonogramPicker />
